da9ed9ad79d895a14124324409437eaaa810baad
Manuel Batsching <m.batsching@abtis.de>
2019-12-18 11:19:46 +0100

    Initial Commit

---
+++ functions/ConvertFrom-MarkdownTable.ps1
@@ -0,0 +1,38 @@
+﻿function ConvertFrom-MarkdownTable
+{
+    [CmdletBinding()]
+    
+    Param
+    (
+        # Hilfebeschreibung zu Param1
+        [Parameter(Position=0)]
+        $InputObject=(Get-Clipboard)
+    )
+    # Use pandoc for markdown parsing
+    $tmpFilePath = ($env:TEMP + [guid]::NewGuid().Guid + '.md')
+    $InputObject | Out-File $tmpFilePath -Encoding utf8
+    $pandocJson = pandoc $tmpFilePath --to json | ConvertFrom-Json
+    $pandocBlocks = $pandocJson.blocks[0].c
+
+    # Simplify pandoc AST json to a simple dictionary
+    $tableDict = [ordered]@{}
+    foreach ($bl in $pandocBlocks[3])
+    {
+        $tableKey = ($bl.C.C -join ' ' -replace ' +',' ')
+        $tableDict.$tableKey = ''
+    }
+
+    $tableHeader = [array]($tableDict.Keys)
+    $tableBlocks = $pandocBlocks[4]
+    for ($i = 0; $i -lt $tableBlocks.Count; $i++) 
+    {
+        $tableRow = $tableBlocks[$i]
+
+        for ($j = 0; $j -lt $tableDict.Count; $j++)
+        {
+            $header = $tableHeader[$j]
+            $tableDict.$header = ($tableRow[$j].C.C -join ' ' -replace ' +',' ')
+        }
+        New-Object -TypeName psobject -Property $tableDict
+    }
+}
---
+++ functions/ConvertTo-MarkdownTable.ps1
@@ -0,0 +1,35 @@
+﻿function ConvertTo-MarkdownTable
+{
+    [CmdletBinding()]
+    
+    Param
+    (
+        # Hilfebeschreibung zu Param1
+        [Parameter(Mandatory, ValueFromPipeline=$true, Position=0)]
+        $InputObject,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateSet('pipe','grid','simple','multiline')]
+        $TableStyle = 'pipe'
+    )
+    Begin 
+    {
+        $objCollection = @()
+    }
+    Process 
+    {
+       # Preserve obj collection when passed through pipeline.
+       foreach ($obj in $InputObject)
+       {
+           $objCollection += $obj
+       }
+       
+    }
+    End 
+    {
+        $tmpFilePath = ($env:TEMP + [guid]::NewGuid().Guid + '.html')
+        $objCollection | ConvertTo-Html -Fragment | Out-File $tmpFilePath -Encoding utf8
+        pandoc $tmpFilePath --to "markdown_strict+${TableStyle}_tables"
+    }
+    
+}
