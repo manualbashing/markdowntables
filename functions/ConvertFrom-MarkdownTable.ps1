7e846293990da5b1ff3c4e0593db9acda2c9e474
Manuel Batsching <m.batsching@abtis.de>
2019-12-18 15:58:58 +0100

    [doc] Added comment based help to functions

--- functions/ConvertFrom-MarkdownTable.ps1
+++ functions/ConvertFrom-MarkdownTable.ps1
@@ -1,21 +1,63 @@
-﻿function ConvertFrom-MarkdownTable
+﻿<#
+.SYNOPSIS
+    Converts a string containing a markdown table into power shell objects.
+.DESCRIPTION
+    Make sure, that the string contains only a single markdown table and nothing else.
+.EXAMPLE
+    PS C:\> $serverData = ConvertFrom-MarkdownTable
+    If no parameters are specified, the markdown table is read from the current clipboard entry.
+.INPUTS
+    A string containing a markdown table
+.OUTPUTS
+    A collection of powershell objects.
+#>
+function ConvertFrom-MarkdownTable
 {
     [CmdletBinding()]
-    
+
     Param
     (
-        # Hilfebeschreibung zu Param1
+        # A string containing the markdown table. Uses clipboard content if left empty.
         [Parameter(Position=0)]
         $InputObject=(Get-Clipboard)
     )
-    # Use pandoc for markdown parsing
+    <#
+    As pandoc already comes with a fantastic parser for markdown, we use it!
+    Pandoc needs a file to process, so we create one in the users temp folder.
+    #>
     $tmpFilePath = ($env:TEMP + [guid]::NewGuid().Guid + '.md')
     $InputObject | Out-File $tmpFilePath -Encoding utf8
     $pandocJson = pandoc $tmpFilePath --to json | ConvertFrom-Json
-    $pandocBlocks = $pandocJson.blocks[0].c
+    $pandocBlocks = $pandocJson.blocks[0].C
 
-    # Simplify pandoc AST json to a simple dictionary
+    <#
+    Pandoc's AST json contains more information as we can stomach. For our purposes
+    we simplify pandoc's AST to a simple dictionary.
+
+    So the following table:
+
+    | Fruit  | Color  | Shape     |
+    |--------|--------|-----------|
+    | Banana | Yellow | Bent      |
+    | Apple  | Red    | Spherical |
+
+    will be simplified to:
+
+    @{
+    Fruit = 'Banana'
+    Color = 'Yellow'
+    Shape = 'Bent'
+    }
+
+    @{
+    Fruit = 'Apple'
+    Color = 'Red'
+    Shape = 'Spherical'
+    }
+
+    #>
     $tableDict = [ordered]@{}
+    # We start with a dictionary, that contains all keys but empty values.
     foreach ($bl in $pandocBlocks[3])
     {
         $tableKey = ($bl.C.C -join ' ' -replace ' +',' ')
@@ -24,7 +66,7 @@
 
     $tableHeader = [array]($tableDict.Keys)
     $tableBlocks = $pandocBlocks[4]
-    for ($i = 0; $i -lt $tableBlocks.Count; $i++) 
+    for ($i = 0; $i -lt $tableBlocks.Count; $i++)
     {
         $tableRow = $tableBlocks[$i]
 
--- functions/ConvertTo-MarkdownTable.ps1
+++ functions/ConvertTo-MarkdownTable.ps1
@@ -1,35 +1,48 @@
-﻿function ConvertTo-MarkdownTable
+﻿<#
+.SYNOPSIS
+    Convert PowerShell objects into a single markdown table.
+.DESCRIPTION
+    Objects usually contain more properties, then you would see on the console. Use `Select-Object` to
+    narrow down the properties, that you want to appear in the final table.
+.EXAMPLE
+    PS C:\> ps | select -Property ProcessName,Id,Handles -First 10  | ConvertTo-MarkdownTable
+    Fill a markdown table with 3 properties from the System.Diagnostics.Process object.
+.INPUTS
+    A collection of PowerShell objects. (Properties should be uniform, use `Select-Object` if they are not.)
+.OUTPUTS
+    A string containing a markdown table, formatted according to the style specified in `-TableStyle`
+#>
+function ConvertTo-MarkdownTable
 {
     [CmdletBinding()]
-    
+
     Param
     (
-        # Hilfebeschreibung zu Param1
+        # The collection of PowerShell objects that will be processed.
         [Parameter(Mandatory, ValueFromPipeline=$true, Position=0)]
-        $InputObject,
+        [System.Object[]]$InputObject,
 
+        # The style of the resulting markdown table. Pandoc allows: 'pipe','grid','simple' and 'multiline'
         [Parameter(Mandatory=$false)]
         [ValidateSet('pipe','grid','simple','multiline')]
-        $TableStyle = 'pipe'
+        [string]$TableStyle = 'pipe'
     )
-    Begin 
+    Begin
     {
         $objCollection = @()
     }
-    Process 
+    Process
     {
-       # Preserve obj collection when passed through pipeline.
-       foreach ($obj in $InputObject)
-       {
-           $objCollection += $obj
-       }
-       
+        # Preserve obj collection when passed through pipeline.
+        foreach ($obj in $InputObject)
+        {
+            $objCollection += $obj
+        }
     }
-    End 
+    End
     {
         $tmpFilePath = ($env:TEMP + [guid]::NewGuid().Guid + '.html')
         $objCollection | ConvertTo-Html -Fragment | Out-File $tmpFilePath -Encoding utf8
         pandoc $tmpFilePath --to "markdown_strict+${TableStyle}_tables"
     }
-    
 }
