fbdb74abb9b1204ef3cf1836086978042ca4109f
Manuel Batsching <m.batsching@abtis.de>
2019-12-18 15:18:08 +0100

    [add] Test if pandoc is installed in Path

--- markdowntables.psm1
+++ markdowntables.psm1
@@ -1,3 +1,15 @@
+try {
+    $pandocVersion = pandoc --version | Select-Object -First 1
+    Write-Verbose "Found $pandocVersion"
+}
+catch [System.Management.Automation.CommandNotFoundException] {
+
+    "Module cannot be imported: Pandoc was not found in path." +
+    " Get your fix here: <https://pandoc.org/installing.html>" |
+        Write-Error
+    break
+}
+
 Get-ChildItem "$PSScriptRoot\functions\*.ps1" |
     ForEach-Object { . $_.FullName }
 
